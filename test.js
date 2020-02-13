17dde04ce21d293429910a036d9ce0f3c4fe8bf9
carine <cd@az.com>
2020-02-13 22:18:15 +0530

    new changes trying

--- test.js
+++ test.js
@@ -40,7 +40,14 @@ describe('Test', function() {
 		await driver.elementByAndroidUIAutomator("new UiSelector().resourceId(\"com.example.allisonsmith:id/clockinout\")").then(target => target.click())
 		await driver.elementByAndroidUIAutomator("new UiSelector().text(\"Manual Clock In/Out\")").then(target => target.click())
 		await driver.elementByAndroidUIAutomator("new UiSelector().text(\"Tej Mhatre\")").then(target => target.click())
+		//await driver.elementByAndroidUIAutomator("new UiSelector().className(\"android.widget.TextView\").text(\"Select\")").then(target => target.click())
 	})
+	
+	it('View Employee detauls' async() => {
+		await driver.elementByAndroidUIAutomator("new UiSelector().className(\"android.widget.TextView\").text(\"View Employee Details\")").then(target => target.click())
+		await driver.elementByAndroidUIAutomator("new UiSelector().className(\"android.view.View\").instance(2)").then(target => target.click())
+	})
+	
 
 	/*it('Check Project Overview', async () => {
 		await driver.elementByAndroidUIAutomator("new UiSelector().resourceId(\"com.example.allisonsmith:id/projectOverview\")").then(target => target.click())
